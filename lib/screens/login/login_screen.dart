b3b4f8095de434a9ef088509dbbd26439d588b20
ikjunchoi <choiikjun1101@gmail.com>
2023-11-22 00:52:20 +0900

    🐛 fix: show apple login for only ios user

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -337,9 +339,10 @@ class _LoginScreenState extends State<LoginScreen> {
                           ],
                         ),
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                      if (Platform.isIOS)
+                        const SizedBox(
+                          height: 10,
+                        ),
                       CustomButton(
                         onPressed: () async {
                           try {
