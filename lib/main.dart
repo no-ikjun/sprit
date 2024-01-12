198fd47c32582571c01a24f2204cfe47e04da08c
ikjunchoi <choiikjun1101@gmail.com>
2024-01-12 17:24:49 +0900

    🍱 update: android build setting

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,12 @@ class MyApp extends StatelessWidget {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: MaterialApp(
+        builder: (context, child) {
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+            child: child!,
+          );
+        },
         debugShowCheckedModeBanner: false,
         title: 'SPRIT',
         //home: const SplashScreen(),
