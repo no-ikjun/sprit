724d9fda2151ec80143e43506dea5e2d114ff312
ikjunchoi <choiikjun1101@gmail.com>
2024-03-17 20:41:58 +0900

    🎨 update: text scaler

--- lib/main.dart
+++ lib/main.dart
@@ -87,7 +87,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         builder: (context, child) {
           return MediaQuery(
-            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+            data: MediaQuery.of(context)
+                .copyWith(textScaler: const TextScaler.linear(1.0)),
             child: child!,
           );
         },
