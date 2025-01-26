2c0a86273da489a2d276c9f8fbe24a3c78e4a0b3
ikjunchoi <choiikjun1101@gmail.com>
2025-01-26 23:06:21 +0900

    ✨ feat: enhance tap feedback with scaling animation

--- lib/widgets/scalable_inkwell.dart
+++ lib/widgets/scalable_inkwell.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class ScalableInkWell extends StatefulWidget {
   final VoidCallback onTap;
@@ -20,32 +19,31 @@ class ScalableInkWell extends StatefulWidget {
 class _ScalableInkWellState extends State<ScalableInkWell> {
   double _scale = 1.0;
 
-  void _onTapDown(TapDownDetails details) {
-    HapticFeedback.lightImpact();
+  Future<void> _handleTap() async {
+    // 눌렸을 때 크기를 작게
     setState(() {
-      _scale = widget.scale; // 눌렀을 때 크기를 5% 줄임
+      _scale = widget.scale;
     });
-  }
 
-  void _onTapUp(TapUpDetails details) {
+    // 애니메이션 대기
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    // 다시 크기를 원래대로
     setState(() {
       _scale = 1.0;
     });
-  }
 
-  void _onTapCancel() {
-    setState(() {
-      _scale = 1.0;
-    });
+    // 애니메이션 대기
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    // onTap 콜백 실행
+    widget.onTap();
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTapDown: _onTapDown,
-      onTapUp: _onTapUp,
-      onTapCancel: _onTapCancel,
-      onTap: widget.onTap,
+      onTap: _handleTap,
       child: AnimatedScale(
         scale: _scale,
         duration: const Duration(milliseconds: 100),
