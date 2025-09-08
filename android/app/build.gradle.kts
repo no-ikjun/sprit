ffc4cabb5212e1af36a977fc28cd00c3fa92c8b0
ikjunchoi <choiikjun1101@gmail.com>
2025-09-08 23:37:30 +0900

    fix: update targetSdk to 35 for improved compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     defaultConfig {
         applicationId = "com.ikjunchoi_android.sprit"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
